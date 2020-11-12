496c1d8e05bff69e4dc1cd594b760c9302011ca5
Axolotlpi <47962588+Axolotlpi@users.noreply.github.com>
2020-11-11 18:08:32 -0600

    Trying out FX
    My first attempt at an FX window in intelliJ.

---
+++ PrettyWindow/src/Window/Main.java
@@ -0,0 +1,56 @@
+package Window;
+
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+import javafx.scene.text.Font;
+
+public class Main extends Application {
+
+    Line makeLine(double x, double y, double x2, double y2){
+        Line line = new Line();
+        line.setStartX(x);
+        line.setStartY(y);
+        line.setEndX(x2);
+        line.setEndY(y2);
+        return line;
+    }
+
+    Text makeText(String message,int x, int y){
+        Text text = new Text();
+        text.setFont(new Font(24));
+        text.setX(x);
+        text.setY(y);
+        text.setText(message);
+        return text;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        int[] dimensions = {1000,600};
+        Line line = makeLine(0,100,dimensions[0],100);
+        Text text = makeText("This is my new Java Application!", dimensions[0]/2, 70);
+
+        Group root = new Group();//FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        ObservableList groups = root.getChildren();
+
+        groups.add(text);
+        groups.add(line);
+
+        Scene primaryScene = new Scene(root, dimensions[0],dimensions[1]);
+        primaryScene.setFill(Color.AQUAMARINE);
+        primaryStage.setTitle("Welcome");
+        primaryStage.setScene(primaryScene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # hello-world
 The place I share to the world
 
-I love coding, guitar, and coffee.
+This is where I begin. I guess if anyone else reads this it could help them somewhat, and if I read this it will be good to reflect on.
+
+Started here:
+https://git-scm.com/book/en/v2
+Good to know the commands, but easier to start with GitHub desktop. The book was still worth reading to gain an understanding of git.
+
+Now, after some cpp and some java stuff I'm going into guis.
+Im starting with JavaFX here:
+https://www.tutorialspoint.com/javafx/index.htm
+
+
 
